83dd7700929fe67ae14072963c03cb7364c4b1d1
Priyanshupbm <priyanshupbm098@gmail.com>
2025-07-10 16:53:50 +0530

    Create Student.java

---
+++ sem_3/Java/Student.java
@@ -0,0 +1,36 @@
+
+class Student {
+  
+    Bank(String n,int a){
+      this.accountnum = num;
+      this.balance = a;
+    }
+    
+    void setter(int a){
+        balance+=a;
+    }
+    
+    void getter(){
+        System.out.println("The account number : "+this.accountnum+"\nBalance : "+this.balance);
+    }
+    
+}
+
+public class Banking {
+    public static void main(String[] args) {
+        System.out.println(" Welcome to the banking\n");
+        
+        Bank u1 = new Bank(6373728,1000);
+        
+        System.out.println("The detail of user 1 is");
+        u1.getter();
+        
+        u1.setter(10000);
+        System.out.println("Detail after adding 10000 to balance is");
+        u1.getter();
+        
+        
+        
+        
+    }
+}
