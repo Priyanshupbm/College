13787a8f74ac48bdaaf2fa32a4bde748942d705b
Priyanshupbm <priyanshupbm098@gmail.com>
2025-07-10 16:52:02 +0530

    Update Banking.java

--- sem_3/Java/Banking.java
+++ sem_3/Java/Banking.java
@@ -1,16 +1,20 @@
 
-
-
-
 class Bank {
-    private int balance = 0;
+  
+  private int accountnum;
+  private int balance ;
+  
+    Bank(int num,int a){
+      this.accountnum = num;
+      this.balance = a;
+    }
     
     void setter(int a){
         balance+=a;
     }
     
-    int getter(){
-        return balance;
+    void getter(){
+        System.out.println("The account number : "+this.accountnum+"\nBalance : "+this.balance);
     }
     
 }
@@ -19,11 +23,15 @@ public class Banking {
     public static void main(String[] args) {
         System.out.println(" Welcome to the banking\n");
         
-        Bank u1 = new Bank();
+        Bank u1 = new Bank(6373728,1000);
+        
+        System.out.println("The detail of user 1 is");
+        u1.getter();
         
         u1.setter(10000);
+        System.out.println("Detail after adding 10000 to balance is");
+        u1.getter();
         
-        System.out.println("The balance of user 1 is "+u1.getter());
         
         
         
